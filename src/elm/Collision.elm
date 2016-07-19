84cc9394dd973b302051e320dc2cb3ef86ca28c3
Chadtech <chadstearns1@gmail.com>
2016-07-19 19:12:21 -0400

    Four spaces to two spaces

--- src/elm/Collision.elm
+++ src/elm/Collision.elm
@@ -1,8 +1,8 @@
 module Collision exposing 
-    ( collision
-    , Mink
-    , Pt
-    )
+  ( collision
+  , Mink
+  , Pt
+  )
 
 {-| this module provides an implementation of the
 Gilbert-Johnson-Keerthi (GJK) collision detection algorith for
@@ -61,11 +61,11 @@ cross2D (ax, ay) (bx, by) = ax*by - ay*bx
  -}
 trip : Pt -> Pt -> Pt -> Pt
 trip a b c =
-    let
-        bac = mul (dot a c) b
-        cab = mul (dot a b) c
-    in
-        sub cab bac
+  let
+    bac = mul (dot a c) b
+    cab = mul (dot a b) c
+  in
+    sub cab bac
 
 -- perpendicular to a in direction of b (2D case)
 perp a b = trip a b a
@@ -79,34 +79,34 @@ isSameDirection a b = (dot a b) > 0
  -}
 calcMinkSupport : Mink a -> Mink b -> (Float, Float) -> (Float,Float) 
 calcMinkSupport (objA, suppA) (objB, suppB) d =
-    let
-        p1 = suppA objA (neg d)
-        p2 = suppB objB d
-     in
-        sub p1 p2
+  let
+    p1 = suppA objA (neg d)
+    p2 = suppB objB d
+   in
+    sub p1 p2
 
 
 -- pass bc as first parameter
 getDirectionVector : Pt -> Pt -> Pt
 getDirectionVector (x1, y1) (x2, y2) =
-    let
-        -- try the triple cross product
-        d = trip (x1,y1) (x2,y2) (x1,y1)
-        collinear = (d == (0,0))
+  let
+    -- try the triple cross product
+    d = trip (x1,y1) (x2,y2) (x1,y1)
+    collinear = (d == (0,0))
 
-        -- bc and c0 are collinear and gave us a 0 cross product.
-        -- inject bc and c into 3d and get ANY perp (the algorithm should
-        -- not care in this case) so now crossing bc=(x1,y1,0) with c=(-x2, -y2, 1)
-        -- remember the second parameter was c0 = -c
-        --
-        -- (u2v3 - u3v2)i - (u1v3 - u3v1)j + (who cares)k = bc x c
-        -- so...
-        -- px = (y1*1 - 0*(-y2)),  py = -(x1*1 - 0*(-x2))   
-        -- px = y1                 py = -x1
-        -- 
-        -- which is a detailed derivation of the obvious :)
-    in
-        if collinear then (y1, -x1) else d 
+    -- bc and c0 are collinear and gave us a 0 cross product.
+    -- inject bc and c into 3d and get ANY perp (the algorithm should
+    -- not care in this case) so now crossing bc=(x1,y1,0) with c=(-x2, -y2, 1)
+    -- remember the second parameter was c0 = -c
+    --
+    -- (u2v3 - u3v2)i - (u1v3 - u3v1)j + (who cares)k = bc x c
+    -- so...
+    -- px = (y1*1 - 0*(-y2)),  py = -(x1*1 - 0*(-x2))   
+    -- px = y1         py = -x1
+    -- 
+    -- which is a detailed derivation of the obvious :)
+  in
+    if collinear then (y1, -x1) else d 
 
 
 {-| Determine if there is a collision between two objects.
@@ -118,25 +118,25 @@ Pt here is used as an alias for (Float, Float). The first argument
 to collision is max recursion depth, which might come in handy in
 the case where you are writing your own support functions.
 
-    poly1 = [(-15,-10),(0,15),(12,-5)] 
-    poly2 = [(-9,13),(6,13),(-2,22)] 
+  poly1 = [(-15,-10),(0,15),(12,-5)] 
+  poly2 = [(-9,13),(6,13),(-2,22)] 
 
-    collision 10 (poly1, polySupport) (poly2, polySupport) == True
+  collision 10 (poly1, polySupport) (poly2, polySupport) == True
 -}
 collision : Int -> Mink a -> Mink b -> Bool
 collision limit minkA minkB =
-    let
-        d1 = (1.0, 0.0)
-        d2 = neg d1
-        c = calcMinkSupport minkA minkB d1 
-        b = calcMinkSupport minkA minkB d2  
-        -- simplex is cb and direction is (cb x c0 x cb)
-        cb = from c b
-        c0 = neg c
-        d = getDirectionVector cb c0
-        (intersects, (sim, newD)) = doSimplex limit 0 minkA minkB ([b,c], d)
-    in
-        intersects
+  let
+    d1 = (1.0, 0.0)
+    d2 = neg d1
+    c = calcMinkSupport minkA minkB d1 
+    b = calcMinkSupport minkA minkB d2  
+    -- simplex is cb and direction is (cb x c0 x cb)
+    cb = from c b
+    c0 = neg c
+    d = getDirectionVector cb c0
+    (intersects, (sim, newD)) = doSimplex limit 0 minkA minkB ([b,c], d)
+  in
+    intersects
 
 
 
@@ -156,20 +156,20 @@ collision limit minkA minkB =
  -}
 doSimplex : Int -> Int -> Mink a  -> Mink b -> (List Pt, Pt) -> (Bool, (List Pt, Pt)) 
 doSimplex limit depth minkA minkB (sim, d) =
-    let
-        a = (calcMinkSupport minkA minkB d)
-        notPastOrig = ((dot a d) < 0)       -- if not past origin, there is no intersection
-        --b = unsafeHead sim
-        (intersects, (newSim, newDir)) = enclosesOrigin a sim
-    in
-        if notPastOrig then 
-            (False, ([], (toFloat depth,toFloat depth)))
-        else if intersects then 
-            (True, (sim, a))
-        else if (depth > limit) then 
-            (False, (newSim, newDir)) 
-        else 
-            doSimplex limit (depth+1) minkA minkB (newSim, newDir)
+  let
+    a = (calcMinkSupport minkA minkB d)
+    notPastOrig = ((dot a d) < 0)     -- if not past origin, there is no intersection
+    --b = unsafeHead sim
+    (intersects, (newSim, newDir)) = enclosesOrigin a sim
+  in
+    if notPastOrig then 
+      (False, ([], (toFloat depth,toFloat depth)))
+    else if intersects then 
+      (True, (sim, a))
+    else if (depth > limit) then 
+      (False, (newSim, newDir)) 
+    else 
+      doSimplex limit (depth+1) minkA minkB (newSim, newDir)
 
 
 {-
@@ -180,10 +180,10 @@ doSimplex limit depth minkA minkB (sim, d) =
  -}
 enclosesOrigin : Pt -> List Pt -> (Bool, (List Pt, Pt))
 enclosesOrigin a sim =
-    case sim of
-        b :: []         -> handle0Simplex a b      -- 0-simplex case
-        b :: c :: []    -> handle1Simplex a b c
-        _               -> (False, (sim,(0,0)))
+  case sim of
+    b :: []     -> handle0Simplex a b    -- 0-simplex case
+    b :: c :: []  -> handle1Simplex a b c
+    _         -> (False, (sim,(0,0)))
 
 
 {-
@@ -191,12 +191,12 @@ enclosesOrigin a sim =
  - (the new simplex will be either [a,b] or [a])
  -}
 handle0Simplex a b =
-    let
-        ab = (from a b)   -- line given by adding our new point
-        a0 = neg a        -- direction of a from the origin 
-        (newSim, newDir) = if (isSameDirection ab a0) then ([a,b], (perp ab a0)) else ([a], a0)
-    in
-        (False, (newSim, newDir))
+  let
+    ab = (from a b)   -- line given by adding our new point
+    a0 = neg a    -- direction of a from the origin 
+    (newSim, newDir) = if (isSameDirection ab a0) then ([a,b], (perp ab a0)) else ([a], a0)
+  in
+    (False, (newSim, newDir))
 
 
 {-
@@ -204,20 +204,20 @@ handle0Simplex a b =
  - origin, or will be replacing the simplex with the closest sub-component to the origin
  -}
 handle1Simplex a b c =
-    let
-        -- a is our new local point of reference
-        a0 = neg a
-        ab = from a b
-        ac = from a c
-        abp = perp ab (neg ac) -- perpendicular to ab facing away from c
-        acp = perp ac (neg ab) -- perpendicular to ac facing away from a
-    in
-        if (isSameDirection abp a0) then -- region 4 or 5
-            if (isSameDirection ab a0) then (False, ([a,b], abp)) else (False, ([a], a0))
-        else if (isSameDirection acp a0) then -- region 6 or 5
-            if (isSameDirection ac a0) then (False, ([a,c], acp)) else (False, ([a], a0)) 
-        else 
-            (True, ([b,c], a0)) 
+  let
+    -- a is our new local point of reference
+    a0 = neg a
+    ab = from a b
+    ac = from a c
+    abp = perp ab (neg ac) -- perpendicular to ab facing away from c
+    acp = perp ac (neg ab) -- perpendicular to ac facing away from a
+  in
+    if (isSameDirection abp a0) then -- region 4 or 5
+      if (isSameDirection ab a0) then (False, ([a,b], abp)) else (False, ([a], a0))
+    else if (isSameDirection acp a0) then -- region 6 or 5
+      if (isSameDirection ac a0) then (False, ([a,c], acp)) else (False, ([a], a0)) 
+    else 
+      (True, ([b,c], a0)) 
 
 
 
